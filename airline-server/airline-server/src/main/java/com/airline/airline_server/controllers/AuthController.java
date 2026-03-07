d9e9d2d640b645ea113b8652abcd705911351a4c
aniket <aniketmeshram275@gmail.com>
2026-03-07 16:29:30 +0530

    updated feature

--- airline-server/airline-server/src/main/java/com/airline/airline_server/controllers/AuthController.java
+++ airline-server/airline-server/src/main/java/com/airline/airline_server/controllers/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/airline/api/auth")
 public class AuthController {
     @Autowired
     private UserService userService;
